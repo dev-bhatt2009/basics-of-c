5f5fa061f1e8bf9c20eaf29882f327c9ac3c2755
dev-bhatt2009 <devbhatt5617@gmail.com>
2026-05-13 19:48:49 +0530

    Create 01_hello_world.c

---
+++ 01_hello_world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>   // Preprocessor directive: includes the standard input-output library
+
+int main()          // Main function: program execution starts here
+{
+    // Print "hello world" to the console
+    printf("hello world");
+
+    // Return 0 to indicate successful program termination
+    return 0;
+}
